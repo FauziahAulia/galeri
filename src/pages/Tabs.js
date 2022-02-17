b39f875f748f793bf5a5fd97988085cb8e254391
FauziahAulia <fauziahaulia1825@gmail.com>
2022-02-17 08:44:48 +0700

    update

--- src/pages/Tabs.js
+++ src/pages/Tabs.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import PropTypes from "prop-types";
-import SwipeableViews from "react-swipeable-views";
 import { useTheme } from "@mui/material/styles";
 // component Material-UI
 import { Tabs, Tab, Typography, Container, Box } from "@mui/material";
@@ -53,47 +52,39 @@ export default function FullWidthTabs() {
   const theme = useTheme();
   const [value, setValue] = React.useState(0);
 
-  const handleChange = (newValue) => {
+  const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
-  const handleChangeIndex = (index) => {
-    setValue(index);
-  };
-
   return (
-    <Container align="center" sx={{ pt: 2 }}>
-      <Box position="static" sx={{ bgcolor: "grey", width: 350 }}>
-        {/* tabs atas  */}
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="primary"
-          textColor="inherit"
-          variant="fullWidth"
-          aria-label="full width tabs example"
-          className="typography"
-        >
-          <Tab label="Foto" {...a11yProps(0)} />
-          <Tab label="Video" {...a11yProps(1)} />
-        </Tabs>
+    <>
+      <Container align="center" sx={{ pt: 2 }}>
+        <Box position="static" sx={{ bgcolor: "grey", width: 350 }}>
+          {/* tabs atas  */}
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            indicatorColor="primary"
+            textColor="inherit"
+            variant="fullWidth"
+            aria-label="full width tabs example"
+            className="typography"
+          >
+            <Tab label="Foto" {...a11yProps(0)} />
+            <Tab label="Video" {...a11yProps(1)} />
+          </Tabs>
 
-        <SwipeableViews
-          axis={theme.direction === "rtl" ? "x-reverse" : "x"}
-          index={value}
-          onChangeIndex={handleChangeIndex}
-        >
           {/* Tabs for Foto */}
-          <TabPanel value={value} index={0} dir={theme.direction}>
+          <TabPanel value={value} index={0}>
             <IsiImage />
           </TabPanel>
 
           {/* Tabs for video */}
-          <TabPanel value={value} index={1} dir={theme.direction}>
+          <TabPanel value={value} index={1}>
             <IsiVideo />
           </TabPanel>
-        </SwipeableViews>
-      </Box>
-    </Container>
+        </Box>
+      </Container>
+    </>
   );
 }
